e70a9bea4a20793bc5f996cb427dac084965b589
NastassiaB98 <nastya.bondar98@mail.ru>
2021-02-04 21:43:16 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n < 0) {
+        n = (-1) * n;
+    }
+    return +n.toString().split("").reverse().join("");
+
 }
